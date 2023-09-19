74b13fd10033dcf24187c643b67d405d0699d92a
Christian <crkohlhoff@gmail.com>
2023-09-19 11:49:33 -0700

    Fixed invalids

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 // The Rizzinator
 // How to hit bitches with charisma
-let a = Math.floor(Math.random() * 11);
-let b = Math.floor(Math.random() * 11);
-let c = Math.floor(Math.random() * 11);
-let d = Math.floor(Math.random() * 11);
+let a = Math.floor(Math.random() * 10);
+let b = Math.floor(Math.random() * 10);
+let c = Math.floor(Math.random() * 10);
+let d = Math.floor(Math.random() * 10);
 const gFactory = (z) => { 
     switch(z){
-        case 1: 
+        case 0: 
         return "Hey";
         break;
-        case 2: 
+        case 1: 
         return "Hello";
         break;
-        case 3: 
+        case 2: 
         return "Oh dear";
         break;
-        case 4: 
+        case 3: 
         return "Behold";
         break;
-        case 5: 
+        case 4: 
         return "Warm tidings";
         break;
-        case 6: 
+        case 5: 
         return "Greetings";
         break;
-        case 7: 
+        case 6: 
         return "Now that I have your attention";
         break;
-        case 8: 
+        case 7: 
         return "You";
         break;
-        case 9: 
+        case 8: 
         return "My Love";
         break;
-        case 10: 
+        case 9: 
         return "My dear";
         break;
         default:
@@ -43,34 +43,34 @@ const gFactory = (z) => {
 }
 const nFactory = (y) => { 
         switch(y){
-            case 1: 
+            case 0: 
             return "dog";
             break;
-            case 2: 
+            case 1: 
             return "cat";
             break;
-            case 3: 
+            case 2: 
             return "heart";
             break;
-            case 4: 
+            case 3: 
             return "brother";
             break;
-            case 5: 
+            case 4: 
             return "sister";
             break;
-            case 6: 
+            case 5: 
             return "arm";
             break;
-            case 7: 
+            case 6: 
             return "number";
             break;
-            case 8: 
+            case 7: 
             return "tuesday";
             break;
-            case 9: 
+            case 8: 
             return "ex";
             break;
-            case 10: 
+            case 9: 
             return "friend";
             break;
             default:
@@ -80,34 +80,34 @@ const nFactory = (y) => {
 }
 const vFactory = (x) => { 
             switch(x){
-                case 1: 
+                case 0: 
                 return "be open emotionally";
                 break;
-                case 2: 
+                case 1: 
                 return "fuck your life up";
                 break;
-                case 3: 
+                case 2: 
                 return "fall in love";
                 break;
-                case 4: 
+                case 3: 
                 return "do the sex";
                 break;
-                case 5: 
+                case 4: 
                 return "go on a date";
                 break;
-                case 6: 
+                case 5: 
                 return "eat the pizza";
                 break;
-                case 7: 
+                case 6: 
                 return "snuggle";
                 break;
-                case 8: 
+                case 7: 
                 return "live life";
                 break;
-                case 9: 
+                case 8: 
                 return "fantasize about a life";
                 break;
-                case 10: 
+                case 9: 
                 return "have kids";
                 break;
                 default:
@@ -117,34 +117,34 @@ const vFactory = (x) => {
         }
 const qFactory = (w) => { 
                 switch(w){
-                    case 1: 
+                    case 0: 
                     return "You down?";
                     break;
-                    case 2: 
+                    case 1: 
                     return "Would you be interested?";
                     break;
-                    case 3: 
+                    case 2: 
                     return "This is what I want";
                     break;
-                    case 4: 
+                    case 3: 
                     return "Would you like that?";
                     break;
-                    case 5: 
+                    case 4: 
                     return "Do you want that?";
                     break;
-                    case 6: 
+                    case 5: 
                     return "Are you to heed the call?";
                     break;
-                    case 7: 
+                    case 6: 
                     return "Will you agree to this?";
                     break;
-                    case 8: 
+                    case 7: 
                     return "Do you find yourself agreeable to such?";
                     break;
-                    case 9: 
+                    case 8: 
                     return "Is that something you are interested in?";
                     break;
-                    case 10: 
+                    case 9: 
                     return "Are you gonna be a good girl?";
                     break;
                     default:
@@ -163,3 +163,7 @@ const rizzinator = () => {
 rizzinator()
 
 
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
